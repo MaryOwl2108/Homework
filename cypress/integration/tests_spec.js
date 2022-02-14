9d24683b715f5e5471389a6058d9406eb4987e42
Serikova_Mary <levdan42108@gmail.com>
2022-02-14 07:34:55 +0200

    solution

--- cypress/integration/tests_spec.js
+++ cypress/integration/tests_spec.js
@@ -22,7 +22,7 @@ const homepage = new Homepage()
 
     it('contacts', () => {
         homepage.contactPage().click();
-        homepage.telNumber().should('contain', 'Телефон: +7 (495) 514-81-35, с 10:00 до 19:00')
+        homepage.telNumber().should('contain', 'НЕ!!!!! Телефон: +7 (495) 514-81-35, с 10:00 до 19:00')
     });
 
 });
